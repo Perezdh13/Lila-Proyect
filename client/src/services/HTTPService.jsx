ff92f3e28218029ae3185c3dc9c491c11195a0f8
pablo perez gonzalez <pabloperezgon91@gmail.com>
2023-04-26 13:05:39 +0200

    create HTTP service

--- client/src/services/HTTPService.jsx
+++ client/src/services/HTTPService.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import axios from 'axios'
+const HTTPService = () => {
 
-function HTTPService() {
-  return (
-    <div>HTTPService</div>
-  )
-}
+  const url = 'http://localhost:8080/';
 
-export default HTTPService
+  const getAllData = async () => {
+    const response = await axios.get(`${url}`);
+    return response.data;
+  };
+
+  const getDataById = async (id) => {
+    const response = await axios.get(`${url}/${id}`, { headers: authHeader() });
+    return response.data;
+  };
+
+  const createData = async (data) => {
+    console.log(authHeader());
+    
+    const response = await axios.post(`${url}`, data, {headers:{"Content-Type" : "application/json",  "Authorization": 'Bearer ' + authHeader()}});
+    return response.data;
+
+  };
+
+  const updateData = async (id, data) => {
+    const response = await axios.put(`${url}/${id}`, data, { headers: authHeader() });
+    return response.data;
+
+  };
+
+  const deleteData = async (id) => {
+    const response = await axios.delete(`${url}/${id}`, { headers: authHeader() });
+    return response.data;
+
+  };
+
+  return {
+    getAllData,
+    getDataById,
+    createData,
+    updateData,
+    deleteData,
+    url
+
+  };
+};
+
+export default HTTPService;
