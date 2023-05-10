86a9c5119cfe47a4c388879c88167c9ba619ddaa
pablo perez gonzalez <pabloperezgon91@gmail.com>
2023-05-10 09:27:09 +0200

    change color

--- client/src/components/admin/controlData/MenuControlData.jsx
+++ client/src/components/admin/controlData/MenuControlData.jsx
@@ -7,7 +7,7 @@ function MenuControlData() {
 
   return (
     <div>
-      <div style={{ backgroundColor: "purple", position: "fixed", width: "15vw", height: "100%", padding: "20px", borderRight: "1px solid #dee2e6" }}>
+      <div style={{ backgroundColor: "#8C52FF", position: "fixed", width: "15vw", height: "100%", padding: "20px", borderRight: "1px solid #dee2e6" }}>
         <div style={{ display: "flex", flexDirection: "column", gap: "5vh", margin: "5vh" }}>
           <Link to="/admin/controlData/womenTable">
             <button className="btn btn-secondary">Gestion de Mujeres</button>
--- client/src/views/admin/Admin.jsx
+++ client/src/views/admin/Admin.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Admin() {
     return (
-        <div style={{ backgroundColor: "purple", position: "fixed", width: "15vw", height: "100%", padding: "20px", borderRight: "1px solid #dee2e6" }}>
+        <div style={{ backgroundColor: "#8C52FF", position: "fixed", width: "15vw", height: "100%", padding: "20px", borderRight: "1px solid #dee2e6" }}>
             <div style={{ display: "flex", flexDirection: "column", gap: "5vh", margin: "5vh" }}>
                 <Link to='singUp'>
                    
