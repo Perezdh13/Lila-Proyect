a8037b7e5d7b2d68833652e83fa9ab97e3073c40
pablo perez gonzalez <pabloperezgon91@gmail.com>
2023-05-12 09:18:58 +0200

    style

--- client/src/components/games/guessWho/AskSeccion.jsx
+++ client/src/components/games/guessWho/AskSeccion.jsx
@@ -4,13 +4,13 @@ import { QuestionsHTTP } from '../../../services/boards/QuestionsHTTP';
 import bubbleLeft from '../../../assets/img/BocadilloIzquierda.png'
 export const QuestionSelectValue = React.createContext();
 function AskSeccion() {
-  const [questions, setQuestions] = useState([]);
-  const [playerQuestion, setPlayerQuestion] = useState('');
-  const [questionType, setQuestionType] = useState('');
-  const [questionValue, setQuestionValue] = useState('');
-  const [isUserTurn, setIsUserTurn] = useState(true);
+  const [questions, setQuestions] = useState([]);console.log(questions);
+  const [playerQuestion, setPlayerQuestion] = useState('');console.log(playerQuestion);
+  const [questionType, setQuestionType] = useState('');console.log(questionType);
+  const [questionValue, setQuestionValue] = useState('');console.log(questionValue);
+  const [isUserTurn, setIsUserTurn] = useState(true);console.log(isUserTurn);
   const [stylePlayerAnswer, setStylePlayerAnswer] = useState({ display: "none" })
-  const [playerAnswer, setPlayerAnswer] = useState('');
+  const [playerAnswer, setPlayerAnswer] = useState('');console.log(playerAnswer);
   const [styleQuestion, setStyleQuestion] = useState({ display: "block" });
   const [styleAnswer, setStyleAnswer] = useState({ display: "none" });
   const [styleUserTurn, setStyleUserTurn] = useState({ display: "block" });
@@ -31,7 +31,7 @@ function AskSeccion() {
     QuestionsHTTP().getQuestions().then((data) => {
       setQuestions(data);
     });
-  }, [])
+  },[])
 
  ////// Custon Event ///////
 
@@ -40,7 +40,7 @@ function AskSeccion() {
       detail: questions
     })
     document.dispatchEvent(questionIA)
-  }, [questions])
+  },[questions])
 
 
   useEffect(() => {
