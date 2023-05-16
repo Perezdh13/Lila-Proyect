d7a4c7ccc055a9aa2187b959807479e9be158587
pablo perez gonzalez <pabloperezgon91@gmail.com>
2023-05-16 09:14:56 +0200

    work ok

--- server/src/main/java/com/app/lilaProject/repository/IRepositoryWoman.java
+++ server/src/main/java/com/app/lilaProject/repository/IRepositoryWoman.java
@@ -3,5 +3,5 @@ package com.app.lilaProject.repository;
 import com.app.lilaProject.Model.CModelWomen;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IRepositoryWoman extends JpaRepository<CModelWomen, Long> {
+public interface IRepositoryWoman extends JpaRepository<CModelWomen, Long>{
 }
