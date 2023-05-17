f287377452b7c92d2de3997e1691faf5ac9724c1
Adrian <adrijaulig@gmail.com>
2023-05-17 14:03:21 +0200

    LearnHomeAddButton

--- client/src/views/learn/LearnHomeView.jsx
+++ client/src/views/learn/LearnHomeView.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+/* import React from 'react'
 import Spain from '../../assets/img/Spain.png'
 import Greta from '../../assets/img/Greta.png'
 import Earth from '../../assets/img/Earth.png'
+import GoBack from '../../assets/img/GoBack.png'
 
 function LearnHomeView() {
   return (
@@ -17,6 +18,44 @@ function LearnHomeView() {
         </div>
       </div>
       <img src={Greta} style={{position:"fixed", marginTop:"45%"}}/>
+      <button>
+            <a href="http://localhost:3000/logged"> <img className='Go Back' src={GoBack} style={{width:"8vh"}}/></a>
+           
+        </button>
+    </div> 
+  )
+}
+
+export default LearnHomeView */
+
+
+
+
+import React from 'react'
+import Spain from '../../assets/img/Spain.png'
+import Greta from '../../assets/img/Greta.png'
+import Earth from '../../assets/img/Earth.png'
+import GoBack from '../../assets/img/GoBack.png'
+
+function LearnHomeView() {
+  return (
+    <div style={{display: "flex", justifyContent:"center", alignItems:"center"}}> 
+      <div className='ContainerImages' style={{display: "flex", justifyContent: "center", alignItems:"center", paddingTop:"10vh"}}>
+        <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", marginLeft:"5%"}}>
+          <img src={Spain} style={{ display: "block", maxWidth: "100%", maxHeight: "100%", width: "70%", marginTop:"3vh"}}/>
+          <p style={{fontSize:"5vh", marginTop:"6vh", marginRight:"12vh"}}>Españolas</p>
+        </div>
+        <div style={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", marginLeft:"15%"}}>
+          <img src={Earth} style={{ display: "block", maxWidth: "100%", maxHeight: "100%", width: "82%", paddingTop:"4%" }}/>
+          <p style={{fontSize:"5vh", marginLeft:"5%"}}>Internacionales</p>
+        </div>
+      </div>
+      <img src={Greta} style={{position:"fixed", bottom: "0vh", right:"40vw"}}/>
+      <button style={{ position:"fixed", bottom: "5vh", right:"5vh", background:"transparent", border:"none", outline:"none"}}>
+        <a href="http://localhost:3000/logged">
+          <img className='Go Back' src={GoBack} style={{width:"8vh"}}/> 
+        </a>
+      </button>
     </div> 
   )
 }
