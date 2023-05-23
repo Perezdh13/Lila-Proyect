86fa35236e77e79f79c6d42077ff8415e38bb71e
gabo113 <g2dgonzalez@gmail.com>
2023-05-23 11:21:37 +0200

    login 3.0

--- client/src/components/admin/singUp/SingUp.jsx
+++ client/src/components/admin/singUp/SingUp.jsx
@@ -1,9 +1,47 @@
+import axios from 'axios';
+import { async } from 'q';
 import React from 'react'
 
-function SingUp() {
-  return (
-    <div>SingUp</div>
-  )
-}
+const SingUp = () => {
+  const [userName, setUserName]= useState("");
+  const[email, setEmail]= useState("");
+  const[password, setPassword]= useState("");
 
+  async function save(event){
+    event.preventDefault();
+    try{
+      await axios.post("http://localhost:8080/user/save")
+    }catch {
+
+    }
+  }
+  
+  
+    return (
+      <div>
+          <form action="">
+              <div>
+              <label htmlFor="">User Name</label>
+              <input type="text" placeholder="Enter your user name" value={userName} onChange={(event)=> {
+                  setUserName(event.target.value)}}/>
+                  </div>
+  
+                  <div>
+              <label htmlFor="">Email</label>
+              <input type="text" placeholder="Enter your user email" value={email} onChange={(event)=> {
+                  setEmail(event.target.value)}}/>
+                  </div>
+  
+                  <div>
+              <label htmlFor="">Password</label>
+              <input type="text" placeholder="Enter your user name" value={password} onChange={(event)=> {
+                  setPassword(event.target.value)}}/>
+                  </div>
+
+                  <button type='submit' onClick={save}>save</button>
+                  
+          </form>
+      </div>
+    )
+  }
 export default SingUp
--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
 
 function Login() {
+  
+  const[email, setEmail]= useState("");
+  const[password, setPassword]= useState("");
+
   return (
-    <div>Login</div>
+    <div> <form action="">
+               <div>
+              <label htmlFor="">Email</label>
+              <input type="text" placeholder="Enter your user email" value={email} onChange={(event)=> {
+                  setEmail(event.target.value)}}/>
+                  </div>
+  
+                  <div>
+              <label htmlFor="">Password</label>
+              <input type="text" placeholder="Enter your user name" value={password} onChange={(event)=> {
+                  setPassword(event.target.value)}}/>
+                  </div>
+
+                  <button type='submit' onClick={Login}>login</button>
+                  
+          </form>
+          </div>
   )
 }
 
